d16d318132902d46c0869339dc0fd0745f06c3f2
JuliSteph2024 <166452755+JuliSteph2024@users.noreply.github.com>
2024-10-06 18:02:10 -0500

    Create r-2.php

---
+++ r-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $numero = 8;
+    if ($numero % 2 == 0) {
+    echo "El número es par.";
+    } else {
+    echo "El número es impar.";
+    }
+?>
+</body>
+</html>
